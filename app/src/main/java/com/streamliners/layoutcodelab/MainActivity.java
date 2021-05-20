67202690171bfa1dca07016acf56423dfe58a24c
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2021-05-20 09:02:14 +0530

    9.1 SharedPrefs

--- app/src/main/java/com/streamliners/layoutcodelab/MainActivity.java
+++ app/src/main/java/com/streamliners/layoutcodelab/MainActivity.java
@@ -1,87 +1,77 @@
 package com.streamliners.layoutcodelab;
 
-import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 import com.streamliners.layoutcodelab.databinding.ActivityMainBinding;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String COUNT = "countKey";
+    private static final String COLOR = "colorKey";
     ActivityMainBinding b;
+    //    Current background color
+    int mColor;
+    //    Current Count
+    int mCount = 0;
+
+    private TextView mShowCountTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        b = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupLayout();
+        mShowCountTextView = findViewById(R.id.count_textview);
+        mColor = ContextCompat.getColor(this, R.color.gray);
+
+        if(savedInstanceState != null){
+            mCount = savedInstanceState.getInt(COUNT);
+            if(mCount != 0){
+                mShowCountTextView.setText(String.format("%s",mCount));
+            }
+            mColor = savedInstanceState.getInt(COLOR);
+            mShowCountTextView.setBackgroundColor(mColor);
+        }
+    }
+
+    private void setupLayout() {
+        b = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
-        setupEventHandlers();
     }
 
-    /**
-     * To give color to the view accordingly
-     * @param view view to be colored
-     */
-    private void setColor(View view) {
-//      Checking ids and setting colors accordingly-------------------------------------------------
-        if (view.getId() == b.boxOneText.getId()) {
-            view.setBackgroundColor(Color.DKGRAY);
-        }
-//        Coloring Box Three to BLue color
-//        Similarly for the following---------------------------------------------------
 
-        else if (view.getId() == b.boxTwoText.getId()) {
-            view.setBackgroundColor(Color.GRAY);
-        }
-
-        else if (view.getId() == b.boxThreeText.getId()) {
-            view.setBackgroundColor(Color.BLUE);
-        }
-
-        else if (view.getId() == b.boxFourText.getId()) {
-            view.setBackgroundColor(Color.MAGENTA);
-        }
-
-        else if (view.getId() == b.boxFiveText.getId()) {
-            view.setBackgroundColor(Color.BLUE);
-        }
-
-//        Checking ids for three buttons and coloring box three, four, five accordingly
-        else if (view.getId() == b.redButton.getId()) {
-            b.boxThreeText.setBackgroundColor(getResources().getColor(R.color.my_red));
-        }
-
-        else if (view.getId() == b.yellowButton.getId()) {
-            b.boxFourText.setBackgroundColor(getResources().getColor(R.color.my_yellow));
-        }
-
-        else if (view.getId() == b.greenButton.getId()) {
-            b.boxFiveText.setBackgroundColor(getResources().getColor(R.color.my_green));
-        }
-
-        else {
-            view.setBackgroundColor(Color.LTGRAY);
-        }
+    public void changeBackground(View view) {
+        int color = ((ColorDrawable) view.getBackground()).getColor();
+        mShowCountTextView.setBackgroundColor(color);
+        mColor = color;
     }
 
-    /**
-     *  Setting up event handlers for views
-     */
-    private void setupEventHandlers() {
+    public void countUp(View view) {
+        mCount++;
+        mShowCountTextView.setText(String.format("%s",mCount));
+    }
 
-//      Making the list of all the clickable views
-        List<View> clickableViews = new ArrayList<>(
-                Arrays.asList(b.boxOneText, b.boxTwoText, b.boxThreeText,
-                        b.boxFourText, b.boxFiveText, b.constraintLayout,
-                        b.redButton, b.yellowButton, b.greenButton)
-        );
+    public void reset(View view) {
+//        Reseting Count
+        mCount = 0;
+        mShowCountTextView.setText(String.format("%s",mCount));
 
-//        Setting all click listeners for the views
-        for (View btn :
-                clickableViews) {
-            btn.setOnClickListener(v -> setColor(btn));
-        }
+//        Reset Color
+        mColor = ContextCompat.getColor(this,R.color.gray);
+        mShowCountTextView.setBackgroundColor(mColor);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(COUNT,mCount);
+        outState.putInt(COLOR,mColor);
     }
 }
